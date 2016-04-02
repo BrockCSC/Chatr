364af0b6da915df81816c0a9b4f4d1544db8c4d3
fzxt <fahadahmad41@hotmail.com>
2016-04-01 20:34:31 -0400

    heroku changes

--- app/components/Index.jsx
+++ app/components/Index.jsx
@@ -3,14 +3,13 @@ import  React, { Component } from 'react';
 import ChatList from './ChatList.jsx';
 import io from 'socket.io-client';
 
-let socket;
+let url = process.env.URL || 'http://localhost:3000';
+const socket = io.connect(url);
 
 class Index extends Component {
 
   constructor(props) {
     super(props);
-    let url = props.location || 'http://localhost:3000';
-    socket = io(url);
     socket.on('connect', () => {
       console.log('connected on client');
     });
