69a950beeb11e93f1ad04ebdab511a1c93ea2b68
fzxt <fahadahmad41@hotmail.com>
2016-04-09 10:24:47 -0400

    fix test props

--- app/components/Index.jsx
+++ app/components/Index.jsx
@@ -1,7 +1,6 @@
 import ChatTextBox from './ChatTextBox.jsx';
 import  React, { Component } from 'react';
 import ChatList from './ChatList.jsx';
-import io from 'socket.io-client';
 
 let socket;
 
--- app/main.js
+++ app/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const socket = io.connect();
 const ReactApp = require('./components/Index.jsx').default;
-const url = `${document.location.protocol}//${document.location.host}`;
 const mountNode = document.getElementById('react-root');
 
 ReactDOM.render(<ReactApp socket = {socket}/>, mountNode);
